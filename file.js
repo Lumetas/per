b294faf13ea8f7b051242e0a884291d8623b55c4
Lumetas <98038223+Lumetas@users.noreply.github.com>
2023-06-19 09:40:56 +0500

    Create file.js

---
+++ file.js
@@ -0,0 +1,42 @@
+const LFile = {};
+
+
+
+LFile.readFile = function (input){
+  let file = input.files[0];
+	
+  let reader = new FileReader();
+
+  reader.readAsText(file);
+
+  reader.onload = function() {
+      sessionStorage.setItem('file', reader.result);
+  };
+
+  reader.onerror = function() {
+    console.log(reader.error);
+  };
+	  }
+	  
+	  
+
+
+
+  
+
+LFile.writeFile = function (string, name) {
+let file = new Blob([string], {
+      type:  "application/json"
+});
+
+let link = document.createElement("a");
+    link.setAttribute("href", URL.createObjectURL(file));
+    link.setAttribute("download", name);
+	link.style.display = 'none';
+    link.textContent = "DOWNLOAD DATA";
+    document.querySelector(".filer").append(link);
+    URL.revokeObjectURL(file);
+	link.click();
+	link.remove()
+
+}
